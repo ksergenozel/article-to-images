53c6b0f9e57c9ddad45d7d2721df21014fc7ef08
K. Sergen ÖZEL <ksergenozel@gmail.com>
2023-03-23 16:22:47 +0300

    improved experience

--- src/App.js
+++ src/App.js
@@ -122,12 +122,10 @@ export default function App() {
         } else {
           setError(true)
           setMessage("Something went wrong. Please try again.")
-          setArticle("")
         }
       } else {
         setError(true)
         setMessage("Something went wrong. Please try again.")
-        setArticle("")
       }
     } else {
       setError(true)
